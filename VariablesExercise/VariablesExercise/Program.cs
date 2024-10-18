af49dcca2045cd6965772f9c1384a17be777a0fb
Grant Hill <granthill@live.com>
2024-10-18 17:26:06 -0400

    Added some README text files + completed the Variables exercise.

---
+++ VariablesExercise/VariablesExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace VariablesExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        //Step # 01;
+            string Derp = "Derp"; //character name.
+            int herpDerp = 101;  //character age.
+            char derpDerp = '!'; //character emotion.
+            bool herpaDerp = true; //character emotion check.
+            double derpaDerp = 79.99; //price character was willing to pay to get launched into the air.
+            decimal herptyDerp = 8000.00m; //how many feet the character was launched into the air.
+        //Step # 02;
+            Console.WriteLine($"{Derp} was a strapping young lad bearing the age of {herpDerp} years old{derpDerp} It's {herpaDerp} that {Derp} was willing to spend ${derpaDerp} to get launched {herptyDerp} feet into the air, by a cannon!");
+        //Step # 03;
+        //Used Crtl+B hotkey to check my work and ensured it had no errors prior to run time.
+        }
+    }
+}
