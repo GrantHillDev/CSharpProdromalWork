27df12c813588881c75dded354af80012c37a464
Grant Hill <granthill@live.com>
2025-03-24 15:32:52 -0400

    TC Encapsulation Exercise Assignment.

---
+++ BankEncapsulation/BankEncapsulation/BankAccount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount()//this is what's known as a constructor -- also known as a special member method. Constructors, like objects, can be used to instantiate the classes from which they're from, and this is a good example of how they don't even need any script written within their scope to enact this notion.
+        {//this is also another way in which a class is instantiated.... so now that this class has been instantiated, we can do stuff with it, without facing runtime errors.
+
+        }
+        private double balance = 0.00;//defining what the field 'balance' is. Fields are like variables in our class. Properties allow us to have more flexibility when working with fields (get; set; as an example) -- allows us to manipulate fields.
+
+        public double Balance//creating and defining a property called, Balance. I noticed that it doesn't seem to matter whether or not the name of this property retains any similarity to any respective field it may or may not be manipulating/eworking with.... so long as whatever you title it here matches what's written into the main method in the other program.
+        {
+            get
+            {
+                return balance;//this is what the property will yield when called in a function, say Console.WriteLine (using dot notation).
+            }
+            set
+            {
+                balance = value;
+            }
+        }
+        public void Deposit(double amount)//(double userDeposit)//this is an instance of what's known as a special member method.... essentially a method that is written within the scope of a class. It defines actions that a class can perform, just like a regular method would (without being written within a class, and therefore not a special member method).
+        {//this method provides the only access a user has to the private field (balance) -- this would be an instance of encapsulation. This allows the user to place a value within this field, but without manipulating it.
+            balance = amount;//here, we've created a method that will accept a double and will then store that value in the balance field. The double has to be called something, and this is easy enough to do through variable declaration.
+            //userDeposit.TryParseConsole.ReadLine();
+        }
+
+        public double GetBalance()//this method returns the amount stored within the balance field.
+        {
+            return balance;
+        }
+
+        public string GetBalanceString()//this method returns the amount stored within the balance field, but takes in strings instead.
+        {
+            return $"{balance}";
+        }
+    }
+
+
+}
---
+++ BankEncapsulation/BankEncapsulation/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace BankEncapsulation
+{
+    public class Program
+    {
+        static void Main(string[] args)//account refers to the name of the instance. Below, we're using a new BankAccount, a new version of our blueprint (class) to create a new object. This will give us access to thing which are made public inside of that class.
+        {//this is an example of how we're creating a new instance of the BankAccount class -- var Account = new BankAccount()// the BankAccount() represents a constructor (so, this is one way to write constructors outside of the class which they're based off of).
+            var Account = new BankAccount();//this is our BankAccount class being instantiated via being called into the main method in the form of an object (even though we still utilize the var declarator here).
+            //Account.Balance = 400;//if we were to have explicitly typed out our instance of the BankAccount class, it would be type BankAccount, but the var declarator already infers that.
+            Account.Balance = 0;
+            //BankAccount Account = new BankAccount();//this is how the inferred typed instance above would be written if it were explicitly typed. All we're doing here is creating custom types (variable types).
+            //Console.WriteLine(Account.Balance);//this syntax works too for the console.WriteLine function, but it only prints its stored integer value to the console without ever suggesting what it is.
+            Console.WriteLine("Presenting a bank account balance that happens to be in your name.");
+            Console.WriteLine("");
+            Console.WriteLine($"Your current account balance is, ${Account.Balance}");//this is where the constructor's get functionality is called and printed to the console. This line of code also more clearly defines what the value of Account.Balance is.
+            Console.WriteLine("");
+            Console.WriteLine("How much would you like to deposit?");
+            Console.WriteLine("");
+            var amountToDeposit = double.Parse(Console.ReadLine());//double.TryParse(Console.ReadLine());for some reason, .TryParse didn't work.... some error regarding method overloading?
+            Console.WriteLine("");//Remember, any input that's taken in by a Console.ReadLine is going to come in the form of a string.
+            Account.Deposit(amountToDeposit);//we defined what the program should do when taking input in the form of a string from a user, by way of storing it within the variable we created above called, amountToDesposit. More flexible and effective way of taking user input rather than just utilizing the Console.ReadLine method by itself.
+            Console.WriteLine("");//what's happening here is very simple, but its mechanics might be confusing. Account.Deposit is the account variable we created using the Deposit method we also defined within its custom BankAccount class which again grants us access to the otherwise private balance field we also defined within the same class, and whether it's because of syntax or otherwise, we set the value of our private balance field equal to a new custom variable called 'amount' within the Deposit method, and the amount variable is going to work with the amountToDesposit variable to not only take in a user's input when prompted for one, but also ensure that it's in the desired format (double type).
+            Console.WriteLine($"Confirmed; your bank account balance now has a total of ${Account.GetBalance()}.");//if this part of the string interpolation was written in without the parentheses at very end of the extension method, like this {Account.GetBalance}, then the script won't print the user's input to the console properly, and ironically, VS community doesn't throw an error for this issue either.... so chances are that syntactic errors just like this one while they are syntactic in nature aren't necessarily denoted as such by VS Community, and it'll be up to us as software developers to trouble shoot these kinds of errors for ourselves.
+            Console.WriteLine("");
+        }//Account.Deposit(amountToDeposit);//Here, Account represents our object, .Deposit is the method extension it's using, and (amountToDeposit) is the variable taking in and parsing the user's input.
+    }//the purpose of writing all of this in such a specific manner is to control a user's access to information; they must go through all of the controlled channels -- this is encapsulation. Access modifiers are the key to achieving this.
+}
